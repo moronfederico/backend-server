3d3d562975f442b0f4ab79cf462072d8a30c170e
Federico Moron <fmoron@cooperativaobrera.coop>
2022-02-14 16:11:22 -0300

    Usuario y token listo

--- app.js
+++ app.js
@@ -2,37 +2,40 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
-
-
-
-
+var bodyParser = require('body-parser');
 
 //Iniciarlizar variables
 var app = express();
 
 
+//Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//Importar Rutas
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 //Conexion a la base de datos
 
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+mongoose.connection.openUri('mongodb://localhost:27017/Hospital2', (err, res) => {
 
-        if (err) throw err;
+    if (err) throw err;
 
-        console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online')
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online')
 
-
-
-    })
-    //Rutas
-
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    })
-})
-
+});
+//Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 //Escuchar peticiones
 app.listen(3000, () => {
     console.log('Express server puerto 3000:\x1b[32m%s\x1b[0m', 'online')
-})
+});
---
+++ middlewares/autenticacion.js
@@ -0,0 +1,38 @@
+var jwt = require('jsonwebtoken');
+
+var SEED = require('../config/config').SEED;
+
+
+
+//=======================================
+//Verificar token
+//=======================================
+
+exports.verificaToken = function(req, res, next) {
+
+    var token = req.query.token;
+
+    jwt.verify(token, SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                mensaje: 'Tokken incorrecto',
+                errors: err
+            });
+
+        }
+
+        req.usuario = decoded.usuario;
+
+
+
+        next();
+        // return res.status(200).json({
+        //     ok: true,
+        //     decoded: decoded
+        // });
+
+    });
+
+}
